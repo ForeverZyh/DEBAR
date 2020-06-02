f3571605cf9e8d3321887c1401423986e3606227
Yuhao Zhang <zyhzyhzyh@pku.edu.cn>
2020-06-02 11:15:46 -0500

    check pick

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import subprocess
 
 from parse.specified_ranges import SpecifiedRanges
 
+path = "/tmp"
+try:
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+
+except:
+    print(
+        "Please run 'python main.py PATH_TO_DATASETS'.\nAborted...")
+    exit(1)
+
 unbounded_weight = False
 unbounded_input = False
 interpreter_path = sys.executable
@@ -14,26 +24,33 @@ result_filename = "results.txt"
 open(result_filename, 'w').close()
 
 for model in SpecifiedRanges.models:
+    print("Running %s" % model)
     if not unbounded_weight and not unbounded_input:
-        os.system("(time %s ./analysis_main.py /newdisk/Yuhao/dnn_test/%s.pbtxt) >> %s 2>&1" % (interpreter_path, model, result_filename))
+        os.system(
+            "(%s ./analysis_main.py %s/%s.pbtxt) >> %s 2>&1" % (interpreter_path, path, model, result_filename))
     elif unbounded_weight:
-        os.system("(time %s ./analysis_main.py /newdisk/Yuhao/dnn_test/%s.pbtxt unbounded_weight) >> %s 2>&1" % (interpreter_path, model, result_filename))
+        os.system("(%s ./analysis_main.py %s/%s.pbtxt unbounded_weight) >> %s 2>&1" % (
+            interpreter_path, path, model, result_filename))
     elif unbounded_input:
-        os.system("(time %s ./analysis_main.py /newdisk/Yuhao/dnn_test/%s.pbtxt unbounded_input) >> %s 2>&1" % (interpreter_path, model, result_filename))
-        
-lines = open(result_filename).readlines()
-times = []
-sats = []
-unsats = []
-for line in lines:
-    if line.find("user") != -1:
-        user_time = float(line[:line.find("user")])
-        line = line[line.find("user") + 5:]
-        sys_time = float(line[:line.find("system")])
-        times.append(user_time + sys_time)
-    if line.find("sat") != -1 and len(line) > 10:
-        splits = line.split()
-        sats.append(int(splits[3]))
-        unsats.append(int(splits[5]))
+        os.system("(%s ./analysis_main.py %s/%s.pbtxt unbounded_input) >> %s 2>&1" % (
+            interpreter_path, path, model, result_filename))
 
-print(sats, unsats, times)
+lines = open(result_filename).readlines()
+# times = []
+info = {}
+for line in lines:
+    # if line.find("user") != -1:
+    #     user_time = float(line[:line.find("user")])
+    #     line = line[line.find("user") + 5:]
+    #     sys_time = float(line[:line.find("system")])
+    #     times.append(user_time + sys_time)
+    if line.find("warnings") != -1 and len(line) > 10:
+        splits = line.split()
+        model_name = splits[0]
+        info[model_name] = line.strip()
+
+for model in SpecifiedRanges.models:
+    if model in info:
+        print(info[model])
+    else:
+        print("Runtime error when running %s." % model)
